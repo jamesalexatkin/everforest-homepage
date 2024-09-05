45cc039161ac646d260b7d38d5a8b7d87a9e6639
James Atkin <curlyw42@gmail.com>
2024-09-05 09:04:50 +0100

    add proper displaying of fg and bg themes

--- js/theme.js
+++ js/theme.js
@@ -1,47 +1,47 @@
-const palettes = {
-    dark: {
-        // Background Colors
-        "bg0": { color: "#2e3b2e", description: "Main background color" },
-        "bg1": { color: "#343F44", description: "Cursor Line Background" },
-        // Add other background colors...
-        // Foreground Colors
-        "fg": { color: "#D3C6AA", description: "Default Foreground, Signs" },
-        "red": { color: "#E67E80", description: "Error Messages" },
-        // Add other foreground colors...
-    },
+const themes = {
     medium: {
-        // Background Colors
-        "bg_dim": { color: "#232A2E", description: "Dimmed Background" },
-        "bg0": { color: "#2D353B", description: "Default Background" },
-        "bg1": { color: "#343F44", description: "Cursor Line Background" },
-        // Add other background colors...
-        // Foreground Colors
-        "fg": { color: "#D3C6AA", description: "Default Foreground, Signs" },
-        "red": { color: "#E67E80", description: "Error Messages" },
-        // Add other foreground colors...
+        // Background
+        "bg_dim": { color: "#232A2E", description: "Dimmed Background", type: "background" },
+        "bg0": { color: "#2D353B", description: "Default Background, Line Numbers Background, Signs Background, Status Line Background (inactive), Tab Line Label (active)", type: "background" },
+        "bg1": { color: "#343F44", description: "Cursor Line Background, Color Columns, Closed Folds Background, Status Line Background (active), Tab Line Background", type: "background" },
+        "bg2": { color: "#3D484D", description: "Popup Menu Background, Floating Window Background, Window Toolbar Background", type: "background" },
+        "bg3": { color: "#475258", description: "List Chars, Special Keys, Tab Line Label Background (inactive)", type: "background" },
+        "bg4": { color: "#4F585E", description: "Window Splits Separators, Whitespaces, Breaks", type: "background" },
+        "bg5": { color: "#56635f", description: "Not currently used", type: "background" },
+        "bg_visual": { color: "#543A48", description: "Visual Selection", type: "background" },
+        "bg_red": { color: "#514045", description: "Diff Deleted Line Background, Error Highlights", type: "background" },
+        "bg_green": { color: "#425047", description: "Diff Added Line Background, Hint Highlights", type: "background" },
+        "bg_blue": { color: "#3A515D", description: "Diff Changed Line Background, Info Highlights", type: "background" },
+        "bg_yellow": { color: "#4D4C43", description: "Warning Highlights", type: "background" },
+        // Foreground
+        "fg": { color: "#D3C6AA", description: "Default Foreground, Signs, [Treesitter: Constants, Variables, Function Parameters, Properties, Symbol Identifiers]", type: "foreground" },
+        "red": { color: "#E67E80", description: "Conditional Keywords, Loop Keywords, Exception Keywords, Inclusion Keywords, Uncategorised Keywords, Diff Deleted Signs, Error Messages, Error Signs", type: "foreground" },
+        "orange": { color: "#E69875", description: "Operator Keywords, Operators, Labels, Storage Classes, Composite Types, Enumerated Types, Tags, Title, Debugging Statements", type: "foreground" },
+        "yellow": { color: "#DBBC7F", description: "Types, Special Characters, Warning Messages, Warning Signs, [Treesitter: Modules, Namespaces]", type: "foreground" },
+        "green": { color: "#A7C080", description: "Function Names, Method Names, Strings, Characters, Hint Messages, Hint Signs, Search Highlights, [Treesitter: Constructors, Function Calls, Built-In Functions, Macro Functions, String Escapes, Regex Literals, Tag Delimiters, Non-Structured Text]", type: "foreground" },
+        "aqua": { color: "#83C092", description: "Constants, Macros, [Treesitter: Strings, Characters]", type: "foreground" },
+        "blue": { color: "#7FBBB3", description: "Identifiers, Uncategorised Special Symbols, Diff Changed Text Background, Info Messages, Info Signs, [Treesitter: Fields, Special Punctuation, Math Environments]", type: "foreground" },
+        "purple": { color: "#D699B6", description: "Booleans, Numbers, Preprocessors, [Treesitter: Built-In Constants, Built-In Variables, Macro-Defined Constants, Attributes/Annotations]", type: "foreground" },
+        "grey0": { color: "#7A8478", description: "Line Numbers, Fold Columns, Concealed Text, Foreground UI Elements", type: "foreground" },
+        "grey1": { color: "#859289", description: "Comments, Punctuation Delimiters, Closed Folds, Ignored/Disabled, UI Borders, Status Line Text", type: "foreground" },
+        "grey2": { color: "#9DA9A0", description: "Cursor Line Number, Tab Line Label (inactive)", type: "foreground" },
+        "statusline1": { color: "#A7C080", description: "Menu Selection Background, Tab Line Label Background (active), Status Line Mode Indicator", type: "foreground" },
+        "statusline2": { color: "#D3C6AA", description: "Status Line Mode Indicator", type: "foreground" },
+        "statusline3": { color: "#E67E80", description: "Status Line Mode Indicator", type: "foreground" },
     },
-    light: {
-        // Background Colors
-        "bg0": { color: "#e5e9f0", description: "Main background color" },
-        "bg1": { color: "#eceff4", description: "Cursor Line Background" },
-        // Add other background colors...
-        // Foreground Colors
-        "fg": { color: "#2e3440", description: "Default Foreground, Signs" },
-        "red": { color: "#bf616a", description: "Error Messages" },
-        // Add other foreground colors...
-    }
 };
+
 
 function renderPalette(theme) {
     const backgroundPaletteContainer = document.getElementById("backgroundPalette");
     const foregroundPaletteContainer = document.getElementById("foregroundPalette");
-    
+
     backgroundPaletteContainer.innerHTML = ''; // Clear previous content
     foregroundPaletteContainer.innerHTML = ''; // Clear previous content
 
-    const colors = palettes[theme];
-    
-    for (const [title, { color, description }] of Object.entries(colors)) {
+    const colors = themes[theme];
+
+    for (const [title, { color, description, type }] of Object.entries(colors)) {
         const card = document.createElement("div");
         card.className = "color-card";
         card.style.backgroundColor = color;
@@ -70,8 +70,8 @@ function renderPalette(theme) {
         card.appendChild(tooltip);
         card.appendChild(feedback);
 
-        // Distinguish between background and foreground colors
-        if (title.startsWith("bg")) {
+        // Use the `type` field to determine which container to append to
+        if (type === "background") {
             backgroundPaletteContainer.appendChild(card);
         } else {
             foregroundPaletteContainer.appendChild(card);
